4ce1a1ce80e90b4f1da1ca295d51e01bbf5d0ae8
Liran Tal <liran.tal@gmail.com>
2017-03-25 21:15:08 +0300

    fix(error): fixed error handling and empty licenses state, updated readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# licensewatch-cli
+# licenseye
+
+Node.js CLI tool to visualize an aggregate list of your dependencies' licenses Edit Add topics
+![licenseye-logo](logo43.svg)
+
+# Related
+
+* [licensewatch](https://github.com/lirantal/licensewatch) - event-emitter Node.js library that recurses a *node_modules/* directory to fetch license information.
+
+# License
+
+MIT @ [Liran Tal](https://github.com/lirantal)
--- index.js
+++ index.js
@@ -12,10 +12,18 @@ const licenses = new LicenseWatch(process.cwd() + '/node_modules/**/package.json
 licenses.fetch()
 
 licenses.on('licensesSummary', (data) => {
+  if (data === undefined || (data && data.maxLicense && data.maxLicense.count === 0)) {
+    return spinner.warn(`Unable to crunch licenses from rainbow: possibly no node_modules/ directory found`)
+  }
+
   // use 1s setTimeout to simulate animation effect
   setTimeout(() => {
     spinner.stop()
     console.log(bars(data.licenses, { bar: chalk.green(figures.square), width: 30, sort: true }))
     spinner.succeed(`Found ${chalk.green(data.maxLicense.name)} with highest count of ${chalk.green(data.maxLicense.count)}`)
   }, 1000)
+})
+
+licenses.on('error', (error) => {
+  spinner.fail(`Unable to crunch licenses from rainbow: ${error}`)
 })
